5157bd94f2af857c8c1ed2e3129c16439ef3a646
Tian Zhou <tzhou@thoughtworks.com>
2018-09-24 23:33:28 +0800

    test @After advice

--- src/main/java/com/thoughtworks/grad/concretepage/aspect/TimeLoggingAspect.java
+++ src/main/java/com/thoughtworks/grad/concretepage/aspect/TimeLoggingAspect.java
@@ -1,15 +1,22 @@
 package com.thoughtworks.grad.concretepage.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+
 @Component
 @Aspect
 public class TimeLoggingAspect {
     @Before("execution(* com.thoughtworks.grad.concretepage.service.*.*(..))")
     public void logBefore() {
         System.out.println("@Before:" + new Date());
+    }
+
+    @After("execution(* com.thoughtworks.grad.concretepage.service.*.*(..))")
+    public void logAfter() {
+        System.out.println("@After:" + new Date());
     }
 }
