3678f18b9225430a121c99b7c530e9fe588933eb
Tian Zhou <tzhou@thoughtworks.com>
2018-09-24 23:36:11 +0800

    test @Around advice

--- src/main/java/com/thoughtworks/grad/concretepage/aspect/TimeLoggingAspect.java
+++ src/main/java/com/thoughtworks/grad/concretepage/aspect/TimeLoggingAspect.java
@@ -1,8 +1,8 @@
 package com.thoughtworks.grad.concretepage.aspect;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,13 +10,20 @@ import java.util.Date;
 @Component
 @Aspect
 public class TimeLoggingAspect {
-    @Before("execution(* com.thoughtworks.grad.concretepage.service.*.*(..))")
-    public void logBefore() {
-        System.out.println("@Before:" + new Date());
-    }
+//    @Before("execution(* com.thoughtworks.grad.concretepage.service.*.*(..))")
+//    public void logBefore() {
+//        System.out.println("@Before:" + new Date());
+//    }
 
-    @After("execution(* com.thoughtworks.grad.concretepage.service.*.*(..))")
-    public void logAfter() {
-        System.out.println("@After:" + new Date());
+//    @After("execution(* com.thoughtworks.grad.concretepage.service.*.*(..))")
+//    public void logAfter() {
+//        System.out.println("@After:" + new Date());
+//    }
+
+    @Around("execution(* com.thoughtworks.grad.concretepage.service.*.*(..))")
+    public void userAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("@Around: Before calculation-" + new Date());
+        joinPoint.proceed();
+        System.out.println("@Around: After calculation-" + new Date());
     }
 }
